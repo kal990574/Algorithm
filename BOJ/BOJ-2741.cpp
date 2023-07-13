1ca3aa4fe19111455ae5464b759ce87f822fa384
mambasa <101706131+kal990574@users.noreply.github.com>
2023-07-13 22:17:09 +0900

    BOJ 2741: print N

---
+++ BOJ/BOJ-2741.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i=1; i<N+1; i++){
+        cout << i << "\n";
+    }
+    return 0;
+}
