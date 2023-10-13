52a8ac769a59a2a70da6fff379611eaaf4b0f9dc
mambasa <101706131+kal990574@users.noreply.github.com>
2023-10-13 16:46:32 +0900

    xcode project insert

---
+++ algorithm/algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 이재현 on 2023/10/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
