0767f2080759452501a1b2061b61e92a5b698a31
mambasa <101706131+kal990574@users.noreply.github.com>
2023-10-31 00:19:49 +0900

    BOJ 2609 Euclid gcd lcm

---
+++ BOJ/BOJ-2609.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    int mul = a * b;
+    cin >> a >> b;
+    int c = max(a,b);
+    int d = min(a,b);
+	int e = c % d;
+	while (e != 0) {
+		c = d;
+		d = e;
+		e = c % d;
+	}
+    int f = a * b / d;
+    cout << d << "\n";
+    cout << f;
+}
