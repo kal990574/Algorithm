86e5e88394ee2fd9cc67cbcaf43ac0b448dac1fc
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-07 18:13:50 +0900

    BOJ 11727 dp 피보나치

---
+++ BOJ/BOJ-11727.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    long long dp[1001] = {0, };
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i=3; i<n+1; i++){
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007;
+    }
+    cout << dp[n];
+}
