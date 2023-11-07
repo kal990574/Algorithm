c9522461ca96e62cc402ac919e145d15e9615e24
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-07 19:23:12 +0900

    BOJ 1654 이분탐색 매개변수 탐색

--- BOJ/BOJ-1654.cpp
+++ BOJ/BOJ-1654.cpp
@@ -3,5 +3,30 @@
 using namespace std;
 
 int main(){
-    
+    vector<long long> v;
+    int K,N;
+    cin >> K >> N;
+    int tmp;
+    int cnt = 0;
+    long long mid = 0;
+    int res = 0;
+    for(int i=0; i<K; i++){
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    long long low = 1;
+    long long high = *max_element(v.begin(), v.end());
+    while(low <= high){
+        mid = (low + high) / 2;
+        for(int i=0; i<v.size(); i++){
+            cnt += (v[i] / mid);
+        }
+        if(cnt < N) high = mid -1;
+        if(cnt >= N) {
+            res = mid;
+            low = mid +1;
+        }
+        cnt = 0;
+    }
+    cout << res;
 }
