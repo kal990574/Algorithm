465d1363a790a6e313fb0821ad3350495b837686
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-13 22:49:53 +0900

    BOJ 11651 vector pair 정렬

--- BOJ/BOJ-11651.cpp
+++ BOJ/BOJ-11651.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N;
+
 int main(){
-    
+    cin >> N;
+    vector< pair<int,int> > arr(N);
+    int x, y;
+    for(int i=0; i<N; i++){
+        cin >> arr[i].second >> arr[i].first;
+    }
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<N; i++){
+        cout << arr[i].second << " " << arr[i].first <<"\n";
+    }
 }
