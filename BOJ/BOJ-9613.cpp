7f20830814058f26d517e316ac6aaac60252b018
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-21 03:08:16 +0900

    BOJ 9613 유클리드 호제법 longlong - silver 4

--- BOJ/BOJ-9613.cpp
+++ BOJ/BOJ-9613.cpp
@@ -1,5 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+int t;
+int n;
+int a,b,c,d;
+long long res;
 int main(){
-    
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> t;
+    for(int i=0;i<t; i++){
+        vector<int> v;
+        cin >> n;
+        for(int j=0; j<n; j++){
+            cin >> c;
+            v.push_back(c);
+        }
+        sort(v.begin(), v.end());
+        for(int j=0; j<n-1; j++){
+            for(int k=j+1; k<n; k++){
+                a = v[j];
+                b = v[k];
+                while(1){
+                d = b % a;
+                b = a;
+                a = d;
+                if(a==0) break;
+                }
+                res += b; 
+            }
+        }
+        cout << res << "\n";
+        res = 0;
+    }
 }
--- BOJ/tempCodeRunnerFile.cpp
+++ BOJ/tempCodeRunnerFile.cpp
@@ -1,19 +1,2 @@
-#include <algorithm>
-#include <iostream>
-using namespace std;
 
-int main(){
-    long long a,b;
-    cin >> a >> b;
-    int c = max(a,b);
-    int d = min(a,b);
-	int e = c % d;
-	while (e != 0) {
-		c = d;
-		d = e;
-		e = c % d;
-	}
-    for(int i=0; i<d; i++){
-        cout << "1";
-    }
-}
+            for(int k=j+1; k<n; k++){
