f56359bcd0cf59e3efedef05d8cefda1f36a7159
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-24 13:40:55 +0900

    BOJ 1475 map 활용 구현 6,9 - silver 5

--- BOJ/BOJ-1475.cpp
+++ BOJ/BOJ-1475.cpp
@@ -1,5 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+map<int, int> m;
 int main(){
-    
+    string str;
+    cin >> str;
+    for(int i=0; i<str.size(); i++){
+        int a = str[i];
+        a = a - '0';
+        if(a == 6 || a == 9) a = 6;
+        m[a] += 1;
+    }
+
+    if(m[6]%2 == 1) m[6] = m[6]/2 + 1;
+    else m[6] = m[6]/2;
+
+    int mx = 0;
+    for(auto i : m){
+        mx = max(mx, i.second);
+    }
+    cout << mx;
 }
