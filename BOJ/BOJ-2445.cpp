19acad039253532d302a0ef60222aa0d4bf2d8b0
mambasa <101706131+kal990574@users.noreply.github.com>
2023-12-08 16:54:15 +0900

    BOJ 2445 별찍기8 - bronze 3

---
+++ BOJ/BOJ-2445.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        for(int j=1; j<N-i; j++){
+            cout << " ";
+        }
+        for(int j=1; j<N-i; j++){
+            cout << " ";
+        }
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int i=1; i<N; i++){
+        for(int j=0; j<N-i; j++){
+            cout << "*";
+        }
+        for(int j=1; j<i+1; j++){
+            cout <<" ";
+        }
+        for(int j=1; j<i+1; j++){
+            cout <<" ";
+        }
+        for(int j=0; j<N-i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
