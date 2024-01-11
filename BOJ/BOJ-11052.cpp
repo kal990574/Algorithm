d9a3a5266d1bd54921810da9dfbaa1bc49174acd
mambasa <101706131+kal990574@users.noreply.github.com>
2024-01-11 19:36:32 +0900

    BOJ 11052 dp 카드 등급

--- BOJ/BOJ-11052.cpp
+++ BOJ/BOJ-11052.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin >> n;
+    vector<int> card(n+1);
+    vector<int> dp(n+1);
+    for(int i=1; i<n+1; i++){
+        cin >> card[i];
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            dp[i] = max(dp[i], dp[i-j] + card[j]);
+        }
+    }
+    cout << dp[n];
 }
