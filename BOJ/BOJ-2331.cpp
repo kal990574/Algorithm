bac6905c7ba5de45e30f2f32852dd8c0a91be44e
mambasa <101706131+kal990574@users.noreply.github.com>
2024-01-15 21:54:31 +0900

    boj 2331 dfs 반복순열

--- BOJ/BOJ-2331.cpp
+++ BOJ/BOJ-2331.cpp
@@ -1,5 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int A, P;
+vector<int> visited(300001);
+
+void dfs(int start){
+    visited[start]++;
+    if(visited[start] == 3)
+        return ;
+    int sum = 0;
+    while(start){
+        sum += (int)pow((start%10),P);
+        start /= 10;
+    }
+    dfs(sum);
+}
 int main(){
-    
+    cin >> A >> P;
+    dfs(A);
+    int res = 0;
+    for(int i=0; i< 300001; i++){
+        if(visited[i] == 1)
+        res ++;
+    }
+    cout << res;
 }
