445ca23ca3920e9ac1ec2b90d3da1b4fdf00c24a
mambasa <101706131+kal990574@users.noreply.github.com>
2024-01-31 19:59:42 +0900

    BOJ 9655 게임이론, dp

--- BOJ/BOJ-9655.cpp
+++ BOJ/BOJ-9655.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int N;
+    cin >> N;
+    if(N%2==0) cout << "CY";
+    if(N%2==1) cout << "SK";
+    /*
+    vector<int> dp(N,0);
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 0;
+    for(int i=4; i<N+1; i++){
+        if(dp[])
+    } */
 }
