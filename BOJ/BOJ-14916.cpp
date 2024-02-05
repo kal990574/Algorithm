f40c5c28f8272f1db8877b422ac7eb594fefe7a4
mambasa <101706131+kal990574@users.noreply.github.com>
2024-02-05 14:45:51 +0900

    BOJ 14916 거스름 돈 그리디

--- BOJ/BOJ-14916.cpp
+++ BOJ/BOJ-14916.cpp
@@ -1,5 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin >> n;
+    int cnt =0;
+    if(n==1 || n==3) cout << "-1";
+    else{
+        while(n > 0){
+            if(n%5==0){
+            n -= 5;
+            cnt ++;
+        }
+        else{
+            n -= 2;
+            cnt ++;
+        }
+    }
+    cout << cnt;
+    }
 }
