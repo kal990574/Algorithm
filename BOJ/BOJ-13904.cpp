bd8c07710aec700e5f3505628f23b002662962e8
mambasa <101706131+kal990574@users.noreply.github.com>
2024-02-06 13:31:37 +0900

    BOJ 9498 단순조건문

---
+++ BOJ/BOJ-13904.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    int tmp = v[0];
+    int idx = 0;
+    while(tmp > *max_element(v.begin(), v.end())){
+        idx ++;
+        v[0] += 1;
+        v[idx] -= 1;
+    }
+}
