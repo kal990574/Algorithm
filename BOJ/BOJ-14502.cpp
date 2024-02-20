5f17b18598f14b61b55790c359a899dd6904358d
mambasa <101706131+kal990574@users.noreply.github.com>
2024-02-20 18:51:14 +0900

    BOJ 14502 연구소 미완성 - gold 4

--- BOJ/BOJ-14502.cpp
+++ BOJ/BOJ-14502.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int M, N;
 vector<vector<int> > visited(8, vector<int>(8));
 vector<vector<int> > maps(8,vector<int>(8));
+vector<vector<int> > maps2(8,vector<int>(8));
 int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
 int cnt = 0;
@@ -15,30 +16,40 @@ void dfs(int startX, int startY){
         int nextY = startY + dy[i];
         if(nextX >=0 && nextX < N && nextY >=0 && nextY < M && maps[nextX][nextY] == 0 && !visited[nextX][nextY]){
             visited[nextX][nextY] = 1;
+            maps[nextX][nextY] = 2;
             dfs(nextX, nextY);
-            
         }
     }
 }
-
 int main(){
     cin >> N >> M;
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin >> maps[i][j];
+            maps2[i][j] = maps[i][j];
         }
     }
-        int cnt = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(cnt == 3) {
-                    dfs(i,j);
-                }
-                if(maps[i][j] == 0) {
-                    maps[i][j] = 1;
-                    cnt ++;
-                }
-            }
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(maps[i][j]==0) 
+        }
     }
-    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(maps[i][j]==2){
+                dfs(i,j);
+            }
+        }
+    }
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(maps[i][j] == 0){
+                cnt++;
+            }
+        }
+    }
+    mx_cnt = max(mx_cnt, cnt);
+
+    cout << mx_cnt;
 }
