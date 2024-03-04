e8419e6f2c95b4cd3c6bc1c83b5919602057107a
mambasa <101706131+kal990574@users.noreply.github.com>
2024-03-04 21:25:09 +0900

    BOJ 7568 덩치 구현 브루트포스 - silver 5

--- BOJ/BOJ-7568.cpp
+++ BOJ/BOJ-7568.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin >> n;
+    int rank = 1;
+    vector<vector<int> > v(n, vector<int>(2));
+    for(int i=0; i<n; i++){
+        cin >> v[i][0] >> v[i][1];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(v[i][0] < v[j][0] && v[i][1] < v[j][1]) rank++;
+        }
+        cout << rank << " ";
+        rank = 1; 
+    }
 }
