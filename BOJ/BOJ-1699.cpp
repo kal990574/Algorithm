b54a9cfb956bae0fe7168f26e2a6816d5a059678
mambasa <101706131+kal990574@users.noreply.github.com>
2024-03-13 13:02:17 +0900

    BOJ 1699 제곱 수의 합 dp - silver 2

--- BOJ/BOJ-1699.cpp
+++ BOJ/BOJ-1699.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 100000
 int main(){
-    
+    int n;
+    cin >> n;
+    // 1 2 3 
+    // 1 2 3 4 2 
+    // 1 2 3 4 2 3 4
+    // 1
+    int dp[MAX+1];
+    for(int i=0; i<=n; i++){
+        dp[i] = i;
+        for(int j=1; j*j <= i; j++){
+            dp[i] = min(dp[i - j*j]+1, dp[i]);
+        }
+    }
+    cout << dp[n];
 }
