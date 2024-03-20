66539c1c494ad435fb27aa642d79c7d6b428c372
mambasa <101706131+kal990574@users.noreply.github.com>
2024-03-20 16:50:58 +0900

    BOJ 13335 트럭 큐 자료구조 - silver 1

--- BOJ/BOJ-13335.cpp
+++ BOJ/BOJ-13335.cpp
@@ -1,10 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
-int main(){
-    int n,w,L;
-    int arr[1001];
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
+
+int n, w, L, val;
+int arr[1000];
+int sum;
+queue<int> q; // 무게
+int cnt = 1;
+int idx = 1;
+
+void traverse()
+{
+    while (idx < n)
+    {
+        cnt++;
+        // 퇴출
+        sum -= q.front();
+        q.pop();
+
+        if (sum + arr[idx] <= L) // 들어올 수 있으면
+        {
+            q.push(arr[idx]);
+            sum += arr[idx];
+            idx++;
+        }
+        else // 들어올 수 없으면
+            q.push(0);
     }
-    
+    return;
+}
+
+int main()
+{
+    // input
+    cin >> n >> w >> L;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // init
+    for (int i = 0; i < w - 1; i++)
+        q.push(0);
+
+    q.push(arr[0]);
+    sum += arr[0];
+
+    traverse();
+
+    cout << cnt + w;
 }
