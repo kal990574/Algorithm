7bd9373c66192f0b457ea240dd8829faf20cb1b2
mambasa <101706131+kal990574@users.noreply.github.com>
2024-03-25 15:42:45 +0900

    BOJ 12904 greedy AB - gold 5

--- BOJ/BOJ-12904.cpp
+++ BOJ/BOJ-12904.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+string s,t;
 int main(){
-    
+    cin >> s >> t;
+    while(s.length()!=t.length()){
+        char tmp = t[t.length()-1];
+        if(tmp=='A'){
+            t.pop_back();
+        }
+        if(tmp=='B'){
+            t.pop_back();
+            reverse(t.begin(),t.end());
+        }
+    }
+    if(s==t) cout << "1";
+    else cout << "0";
 }
