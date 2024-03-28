5962900e6e2731b93a5649e00e02dad4b10bba1c
mambasa <101706131+kal990574@users.noreply.github.com>
2024-03-28 19:15:41 +0900

    BOJ 9084 구상중

---
+++ BOJ/BOJ-9084.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T, N, M;
+int c[21];
+int dp[100001][20];
+int main(){
+    cin >> T;
+    while(T){
+        for(int i=0; i<N; i++){
+            cin >> c[i];
+        }
+        cin >> M;
+
+        T--;
+    }
+}
