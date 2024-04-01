f8ef67d8d4793667335fbd98eaecfead10579ad2
mambasa <101706131+kal990574@users.noreply.github.com>
2024-04-01 13:34:50 +0900

    BOJ 9084 동전 dp knapsack - gold 5

--- BOJ/BOJ-9084.cpp
+++ BOJ/BOJ-9084.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 int T, N, M;
-int c[21];
-int dp[100001][20];
 int main(){
     cin >> T;
     while(T){
+        int c[21] = {0, };
+        int dp[10001] = {0, };
+        cin >> N;
         for(int i=0; i<N; i++){
             cin >> c[i];
         }
         cin >> M;
-
+        // 1 2 2 3
+        dp[0] = 1;
+        for(int i=0; i<N; i++){
+            for(int j=c[i]; j<=M; j++){
+                dp[j] += dp[j-c[i]];
+            }
+        }
+        cout << dp[M] << "\n";
         T--;
     }
 }
