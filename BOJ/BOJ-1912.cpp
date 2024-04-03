ee31416cfa69138e29ce28e4766cba3dfd9c3815
mambasa <101706131+kal990574@users.noreply.github.com>
2024-04-03 13:56:20 +0900

    BOJ 1912 연속합 dp - silver2 완성

--- BOJ/BOJ-1912.cpp
+++ BOJ/BOJ-1912.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+int arr[100001];
+int dp[100001];
+int res;
 int main(){
-    
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> arr[i];
+    }
+    //dp[1][0] = 0;
+    dp[1] = arr[1];
+    res = arr[1];
+    for(int i=2; i<=n; i++){
+        //dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+        //dp[i][1] = max(dp[i][0]+arr[i], arr[i]);
+        //선택 x
+        dp[i] = max(arr[i], dp[i-1] + arr[i]);
+    }
+    for(int i=1; i<=n; i++){
+        res = max(res, dp[i]);
+    }
+    cout << res;
 }
