f0576938bd052e236eb463c94fc6ba762c708be9
mambasa <101706131+kal990574@users.noreply.github.com>
2024-04-08 14:06:00 +0900

    BOJ 2133 타일 dp - gold 4

--- BOJ/BOJ-2133.cpp
+++ BOJ/BOJ-2133.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int dp[31];
+int arr[31];
 int N;
 int main(){
     cin >> N;
@@ -8,7 +9,10 @@ int main(){
     dp[0] = 1;
     dp[2] = 3;
     for(int i=4; i<=N; i+=2){
-        dp[i] = dp[i-2] * 3;
+        dp[i] = dp[i-2] * dp[2];
+        for(int j=4; j<=i; j+=2){
+            dp[i] += dp[i-j] * 2;
+        }
     }
     cout << dp[N];
 }
