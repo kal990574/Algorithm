853efa1b734885278368729400271c68bcdfa71e
mambasa <101706131+kal990574@users.noreply.github.com>
2024-04-17 16:00:02 +0900

    BOJ 14503 로봇청소기 구현 시뮬레이션 - gold 5

--- BOJ/BOJ-14503.cpp
+++ BOJ/BOJ-14503.cpp
@@ -1,5 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N,M;
+int start_x, start_y;
+int direction;
+int maps[51][51];
+int clean_cnt = 0;
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+
+bool range(int x, int y){
+    if(x >= 0 && x < N && y >= 0 && y < M) return true;
+    else return false;
+}
+
+int change_dir(int dir){
+    dir--;
+    if(dir == -1) dir = 3;
+    return dir;
+}
+
+int back_dir(int dir){
+    int back;
+    back = dir-2;
+    if(back == -2) back = 2;
+    if(back == -1) back = 3;
+    return back;
+}
+
+void clean(int sx, int sy, int dir){
+    int flag = 0;
+    if(range(sx,sy) && maps[sx][sy] == 0){
+        maps[sx][sy] = 2;
+        clean_cnt ++;
+        //cout << sx << " " << sy << "\n";
+    }
+
+    for(int i=0; i<4; i++){
+        if(range(sx+dx[i], sy+dy[i]) && maps[sx+dx[i]][sy+dy[i]] == 0){
+            flag++;
+            dir = change_dir(dir);
+            if(range(sx+dx[dir], sy+dy[dir]) && maps[sx+dx[dir]][sy+dy[dir]] == 0){
+                clean(sx+dx[dir], sy+dy[dir], dir);
+                return ;
+            }
+            else {
+                clean(sx,sy,dir);
+                return ;
+            }
+        }
+    }
+
+    if(!flag){
+        int back = back_dir(dir);
+        if(range(sx+dx[back], sy+dy[back]) && maps[sx+dx[back]][sy+dy[back]] != 1) {
+            clean(sx+dx[back],sy+dy[back], dir);
+            return ;
+        }
+        if(range(sx+dx[back], sy+dy[back]) && maps[sx+dx[back]][sy+dy[back]] == 1) return ;
+    }
+}
+
 int main(){
-    
+    cin >> N >> M;
+    cin >> start_x >> start_y >> direction;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> maps[i][j];
+        }
+    }
+    clean(start_x, start_y, direction);
+    cout << clean_cnt;
 }
--- BOJ/NS_assignment_2.py
+++ BOJ/NS_assignment_2.py
@@ -1,4 +1,13 @@
-# 함수: 소수 판별
+# 난수 생성을 위한 random 라이브러리 import
+import random
+
+# 유클리드 알고리즘 활용 최대 공약수 계산 함수 gcd() 정의
+def gcd(num1, num2):
+    while num2 != 0:
+        num1, num2 = num2, num1 % num2
+    return num1
+
+# 소수 판별 함수 is_prime() 정의
 def is_prime(num):
     if num <= 1:
         return False
@@ -13,29 +22,24 @@ def is_prime(num):
         i += 6
     return True
 
-# 함수: 두 소수(p, q) 입력받기
+# 소수 p,q 입력 함수 input_primes() 정의
 def input_primes():
     while True:
         p = int(input("소수 p를 입력하세요: "))
         if is_prime(p):
             break
         else:
-            print("입력한 수가 소수가 아닙니다. 다시 입력하세요.")
+            print("입력한 수가 소수가 아닙니다. 소수를 다시 입력해주세요.")
+
     while True:
         q = int(input("소수 q를 입력하세요: "))
         if is_prime(q) and q != p:
             break
         else:
-            print("입력한 수가 소수가 아니거나 p와 동일합니다. 다시 입력하세요.")
+            print("입력한 수가 소수가 아니거나 p와 동일합니다. 소수를 다시 입력해주세요.")
     return p, q
 
-# 함수: 최대공약수 구하기
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-# 함수: 모듈러 역원 구하기
+# 확장 유클리드 알고리즘 활용 모듈러 역수 계산 함수 mod_inverse() 정의
 def mod_inverse(a, m):
     m0, x0, x1 = m, 0, 1
     while a > 1:
@@ -44,35 +48,56 @@ def mod_inverse(a, m):
         x0, x1 = x1 - q * x0, x0
     return x1 + m0 if x1 < 0 else x1
 
-# 함수: 키 쌍 생성
+# 오일러 피 함수값 계산 함수 euler_phi() 정의
+def euler_phi(n):
+    result = n
+    p = 2
+    while p * p <= n:
+        if n % p == 0:
+            while n % p == 0:
+                n //= p
+            result -= result // p
+        p += 1
+    if n > 1:  # n이 소수인 경우
+        result -= result // n
+    return result
+
+# RSA 키 쌍 생성 함수 generate_keys() 정의
 def generate_keys(p, q):
     n = p * q
-    phi = (p - 1) * (q - 1)
-    e = 65537  # 공개키
-    d = mod_inverse(e, phi)  # 개인키
+    phi = euler_phi(n)
+
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    d = mod_inverse(e, phi)
     return ((e, n), (d, n))
 
-# 함수: 암호화
+# RSA 알고리즘 암호화 함수 정의
 def encrypt(plaintext, public_key):
-    e, n = public_key
-    encrypted = [pow(ord(char), e, n) for char in plaintext]
+    key, n = public_key
+    encrypted = [pow(ord(char), key, n) for char in plaintext]
     return encrypted
 
-# 함수: 복호화
+# RSA 알고리즘 복호화 함수 정의
 def decrypt(ciphertext, private_key):
-    d, n = private_key
-    decrypted = [chr(pow(char, d, n)) for char in ciphertext]
+    key, n = private_key
+    decrypted = [chr(pow(char, key, n)) for char in ciphertext]
     return ''.join(decrypted)
 
-# 메인 함수
+# 메인 함수 정의
 def main():
-    print("RSA 알고리즘을 이용한 암호화/복호화 프로그램")
+    print("RSA 알고리즘 활용 암호화/복호화 프로그램")
     p, q = input_primes()
     public_key, private_key = generate_keys(p, q)
 
     plaintext = input("암호화할 메시지를 입력하세요: ")
     encrypted_message = encrypt(plaintext, public_key)
-    print("암호화된 메시지:", encrypted_message)
+    print("암호화된 메시지:", ''.join(map(lambda x: str(x), encrypted_message)))
 
     decrypted_message = decrypt(encrypted_message, private_key)
     print("복호화된 메시지:", decrypted_message)
