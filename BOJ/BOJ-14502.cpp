79afa034b35f8194c740b73fe83549eb10d7dd68
mambasa <101706131+kal990574@users.noreply.github.com>
2024-04-20 20:28:02 +0900

    BOJ 14502 연구소 브루트포스 bfs - gold 4

--- BOJ/BOJ-14502.cpp
+++ BOJ/BOJ-14502.cpp
@@ -1,55 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
 int M, N;
-vector<vector<int> > visited(8, vector<int>(8));
-vector<vector<int> > maps(8,vector<int>(8));
-vector<vector<int> > maps2(8,vector<int>(8));
+int visited[8][8];
+int maps[8][8];
+int copy_maps[8][8];
 int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
-int cnt = 0;
-int mx_cnt = 0;
+int res = 0;
 
-void dfs(int startX, int startY){
-    visited[startX][startY] = 1;
-    for(int i=0; i<4; i++){
-        int nextX = startX + dx[i];
-        int nextY = startY + dy[i];
-        if(nextX >=0 && nextX < N && nextY >=0 && nextY < M && maps[nextX][nextY] == 0 && !visited[nextX][nextY]){
-            visited[nextX][nextY] = 1;
-            maps[nextX][nextY] = 2;
-            dfs(nextX, nextY);
-        }
-    }
+bool range(int x, int y){
+    if(x>=0 && x<N && y>=0 && y<M) return true;
+    else return false;
 }
-int main(){
-    cin >> N >> M;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            cin >> maps[i][j];
-            maps2[i][j] = maps[i][j];
-        }
-    }
 
+void bfs(){
+    memset(visited, 0, sizeof(visited));
+    queue<pair<int, int> > q;
+    int spread[8][8];
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            if(maps[i][j]==0) 
+            spread[i][j] = maps[i][j];
         }
     }
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            if(maps[i][j]==2){
-                dfs(i,j);
+            if(spread[i][j]==2 && !visited[i][j]){
+                q.push(make_pair(i,j));
+                //cout << i <<" "<< j << "\n";
+                visited[i][j] = 1;
+                while(!q.empty()){
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    for(int k=0; k<4; k++){
+                        int nx = x+dx[k];
+                        int ny = y+dy[k];
+                        if(range(nx,ny) && !visited[nx][ny] && spread[nx][ny] == 0){
+                            visited[nx][ny] = 1;
+                            spread[nx][ny] = 2;
+                            q.push(make_pair(nx,ny));
+                        }
+                    }
+                }
             }
         }
+    }
+    int cnt = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(spread[i][j] == 0)
+                cnt ++;
+        }
+    }
+    res = max(cnt, res);
+    //cout << cnt << "\n";
+}
+
+void insert_wall(int cnt){
+    if(cnt==3){
+        bfs();
+        return;
     }
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             if(maps[i][j] == 0){
-                cnt++;
+                maps[i][j] = 1;
+                insert_wall(cnt+1);
+                maps[i][j] = 0;
             }
         }
     }
-    mx_cnt = max(mx_cnt, cnt);
+}
 
-    cout << mx_cnt;
+int main(){
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> copy_maps[i][j];
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(copy_maps[i][j] == 0){
+                for(int k=0; k<N; k++){
+                    for(int l=0; l<M; l++){
+                        maps[k][l] = copy_maps[k][l];
+                    }
+                }
+                maps[i][j] = 1;
+                insert_wall(1);
+                maps[i][j] = 0;
+            }
+        }
+    }
+    cout << res;
 }
