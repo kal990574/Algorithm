7e371de5200da5b89597139892410ed5912ef52c
mambasa <101706131+kal990574@users.noreply.github.com>
2024-05-23 03:55:57 +0900

    ns assignment 4: server clear

--- BOJ/NS_Assignment/Client_201820690_이재현.py
+++ BOJ/NS_Assignment/Client_201820690_이재현.py
@@ -27,51 +27,74 @@ def aes_decrypt(ct, key):
     pt = unpad(cipher.decrypt(ct), AES.block_size)
     return pt
 
+# 데이터 전송 함수
+def send_data(sock, data):
+    length = len(data)
+    sock.sendall(length.to_bytes(4, 'big'))
+    sock.sendall(data)
+
+# 데이터 수신 함수
+def recv_data(sock):
+    length = int.from_bytes(sock.recv(4), 'big')
+    return sock.recv(length)
+
 # 클라이언트 소켓 설정
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('localhost', 9999))
 
     # 서버로부터 RSA 공개키 수신
-    server_public_key = RSA.import_key(client.recv(1024))
+    server_public_key = RSA.import_key(recv_data(client))
+    #server_public_key = RSA.import_key(client.recv(1024))
+    print("RSA 공개키 수신")
 
     # 서버로부터 DHE 파라미터 및 공개키 수신
-    n = int(client.recv(1024).decode())
-    g = int(client.recv(1024).decode())
-    server_dhe = int(client.recv(1024).decode())
+    G = int.from_bytes(recv_data(client), 'big')
+    #g = int(client.recv(2048).decode())
+    print("g 수신")
+    N = int.from_bytes(recv_data(client), 'big')
+    #n = int(client.recv(2048).decode())
+    print("n 수신")
+    server_dhe = int.from_bytes(recv_data(client), 'big')
+    #server_dhe = int(client.recv(2048).decode())
+    print("dhe 공개키 수신")
 
     # 서버로부터 RSA 암호화된 DHE 파라미터 및 공개키 수신
-    encrypted_data = client.recv(256)
+    encrypted_data = recv_data(client)
     cipher_rsa = PKCS1_OAEP.new(server_public_key)
-    decrypted_data = cipher_rsa.decrypt(encrypted_data).decode()
-    n_dec, g_dec, server_dhe_dec = map(int, decrypted_data.split(','))
+    decrypted_data = cipher_rsa.decrypt(encrypted_data)
+    decrypted_data_str = decrypted_data.decode()
+    G_dec, N_dec, server_dhe_dec = map(int, decrypted_data_str.split(','))
+    print("rsa 서명 데이터 수신")
 
     # DHE 파라미터 및 공개키의 무결성 검증
-    if n == n_dec and g == g_dec and server_dhe == server_dhe_dec:
+    if G == G_dec and N == N_dec and server_dhe == server_dhe_dec:
         print("서버로부터 받은 DHE 파라미터 및 공개키가 검증되었습니다.")
     else:
         print("서버로부터 받은 DHE 파라미터 및 공개키가 일치하지 않습니다.")
         client.close()
         return
+    
 
     # 클라이언트의 DHE 공개키 생성
     client_private = get_random_bytes(32)
-    client_dhe = pow(g, int.from_bytes(client_private, 'big'), n)
+    client_dhe = pow(G, int.from_bytes(client_private, 'big'), N)
     client_dhe_bytes = client_dhe.to_bytes(256, 'big')
 
     # RSA로 클라이언트의 DHE 공개키 암호화 및 전송
-    encrypted_client_dhe = cipher_rsa.encrypt(client_dhe_bytes)
-    client.send(encrypted_client_dhe)
+    send_data(client, client_dhe_bytes)
+    #encrypted_client_dhe = cipher_rsa.encrypt(client_dhe_bytes)
+    #client.send(encrypted_client_dhe)
 
     # 공유 비밀키 생성
-    shared_key = pow(server_dhe, int.from_bytes(client_private, 'big'), n)
+    shared_key = pow(server_dhe, int.from_bytes(client_private, 'big'), N)
     shared_key = shared_key.to_bytes(32, 'big')
 
     # AES 키 생성 및 메시지 암호화 후 전송
     aes_key = scrypt(shared_key, b'salt', 32, N=2**14, r=8, p=1)
     message = input("서버에 보낼 메시지를 입력하세요: ").encode()
     encrypted_msg = aes_encrypt(message, aes_key)
-    client.send(encrypted_msg)
+    send_data(client, encrypted_msg)
 
     client.close()
 
--- BOJ/NS_Assignment/Server_201820690_이재현.py
+++ BOJ/NS_Assignment/Server_201820690_이재현.py
@@ -29,12 +29,20 @@ def aes_decrypt(ct, key):
 
 # DHE 파라미터 생성
 def generate_dhe_params():
-    n = int('FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1'
-            '29024E088A67CC74020BBEA63B139B22514A08798E3404DD'
-            'EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245'
-            'E485B576625E7EC6F44C42E9A63A3620FFFFFFFFFFFFFFFF', 16)
-    g = 2
-    return n, g
+    G = 2
+    N = int('F'*64, 16)
+    return G, N
+
+# 데이터 전송 함수
+def send_data(sock, data):
+    length = len(data)
+    sock.sendall(length.to_bytes(4, 'big'))
+    sock.sendall(data)
+
+# 데이터 수신 함수
+def recv_data(sock):
+    length = int.from_bytes(sock.recv(4), 'big')
+    return sock.recv(length)
 
 # 서버 소켓 설정
 def start_server():
@@ -51,40 +59,49 @@ def start_server():
         private_key, public_key = generate_rsa_keys()
 
         # DHE 파라미터 생성
-        n, g = generate_dhe_params()
+        G, N = generate_dhe_params()
 
         # 서버의 DHE 공개키 생성
         server_private = get_random_bytes(32)
-        server_dhe = pow(g, int.from_bytes(server_private, 'big'), n)
+        server_dhe = pow(G, int.from_bytes(server_private, 'big'), N)
 
 
         # DHE 파라미터 및 공개키를 RSA로 암호화하여 클라이언트에게 전송
-        data_to_encrypt = f"{n},{g},{server_dhe}".encode()
+        data_to_encrypt = f"{G},{N},{server_dhe}".encode()
         cipher_rsa = PKCS1_OAEP.new(RSA.import_key(private_key))
         encrypted_data = cipher_rsa.encrypt(data_to_encrypt)
 
         # RSA 공개키, DHE 파라미터, DHE 공개키를 클라이언트에게 전송
-        client_socket.send(public_key)
-        client_socket.send(str(n).encode())
-        client_socket.send(str(g).encode())
-        client_socket.send(str(server_dhe).encode())
-        client_socket.send(encrypted_data)
+        send_data(client_socket, public_key)
+        send_data(client_socket, G.to_bytes((G.bit_length() + 7) // 8, 'big'))
+        send_data(client_socket, N.to_bytes((N.bit_length() + 7) // 8, 'big'))
+        send_data(client_socket, server_dhe.to_bytes((server_dhe.bit_length() + 7) // 8, 'big'))
+        send_data(client_socket, encrypted_data)
+        #client_socket.send(str(n).encode())
+        #client_socket.send(str(g).encode())
+        #client_socket.send(str(server_dhe).encode())
+        #client_socket.send(encrypted_data)
 
         # 클라이언트로부터 RSA 암호화된 DHE 공개키 수신
-        encrypted_client_dhe = client_socket.recv(256)
-        client_dhe = int.from_bytes(cipher_rsa.decrypt(encrypted_client_dhe), 'big')
+        client_dhe = recv_data(client_socket)
+        client_dhe = int.from_bytes(client_dhe, 'big')
+        #encrypted_client_dhe = client_socket.recv(256)
+        #client_dhe = int.from_bytes(cipher_rsa.decrypt(encrypted_client_dhe), 'big')
 
         # 공유 비밀키 생성
-        shared_key = pow(client_dhe, int.from_bytes(server_private, 'big'), n)
+        shared_key = pow(client_dhe, int.from_bytes(server_private, 'big'), N)
         shared_key = shared_key.to_bytes(32, 'big')
 
         # 클라이언트로부터 암호화된 메시지 수신 및 복호화
-        client_encrypted_msg = client_socket.recv(2048)
+        client_encrypted_msg = recv_data(client_socket)
+        #client_encrypted_msg = client_socket.recv(2048)
         aes_key = scrypt(shared_key, b'salt', 32, N=2**14, r=8, p=1)
         decrypted_msg = aes_decrypt(client_encrypted_msg, aes_key)
         print("클라이언트로부터 받은 메시지: ", decrypted_msg.decode())
 
         client_socket.close()
+        server.close()
+        break
 
 if __name__ == '__main__':
     start_server()
