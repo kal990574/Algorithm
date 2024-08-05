bd13f90ad187c1f4d2f2781b8e2af04b9af86262
mambasa <101706131+kal990574@users.noreply.github.com>
2024-08-05 22:40:39 +0900

    BOJ 15989 코테 유형 dp - gold 5

---
+++ BOJ/BOJ-15989.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T;
+int n;
+int dp[10001][4];
+int main(){
+    cin >> T;
+    dp[1][1] = 1;
+    dp[2][1] = 1;
+    dp[2][2] = 1;
+    dp[3][1] = 1;
+    dp[3][2] = 1;
+    dp[3][3] = 1;
+    for(int i=4; i<=10000; i++){
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][2] + dp[i-2][1];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3]; 
+        }
+    while(T--){
+        cin >> n;
+        cout << dp[n][1] + dp[n][2] + dp[n][3] << "\n";
+    }
+}
