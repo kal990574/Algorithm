7dcb4271a863dc1e8e50c0b5717d244141d6fe87
mambasa <101706131+kal990574@users.noreply.github.com>
2024-08-16 17:47:40 +0900

    BOJ 2493 코테 유형 스택 - gold 5

--- BOJ/BOJ-2493.cpp
+++ BOJ/BOJ-2493.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
-int tower[500000];
-int res[500000];
+stack<pair<int, int> > stk;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cin >> N;
-    for(int i=0; i<N; i++){
-        cin >> tower[i];
-    }
-    for(int i=N; i>=0; i--){
-        for(int j=i-1; j>=0; j--){
-            if(tower[i] <= tower[j]){
-                res[i] = j+1;
+    for(int i=1; i<=N; i++){
+        int tmp;
+        cin >> tmp;
+        while(!stk.empty()){
+            if(stk.top().second > tmp){
+                cout << stk.top().first << " ";
                 break;
             }
+            stk.pop();
         }
-    }
-    for(int i=0; i<N; i++){
-        cout << res[i];
+        if(stk.empty()){
+            cout << "0 ";
+        }
+        stk.push(make_pair(i, tmp));
     }
 }
