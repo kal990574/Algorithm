e19d7610a0a6905215806785242fda703bd76f46
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-04 16:24:28 +0900

    BOJ 2668 코테 유형 dfs - gold 5 clear

--- BOJ/BOJ-2668.cpp
+++ BOJ/BOJ-2668.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
+vector<int> v(101);
+int visited[101];
 vector<int> cycle;
-vector<int> v(N+1);
-vector<int> visited(N+1);
-vector<int> same;
 int in_cycle = 0;
 
 void dfs(int top, int start){
+    if(visited[top]){
+        if(top == start) cycle.push_back(top);
+        return;
+    }
     visited[top] = 1;
-    int bottom = v[top];
-    cycle.push_back(top);
-    if(bottom == start){
-        in_cycle = 1;
-        return ;
-    }
-    if(!visited[bottom]){
-        dfs(bottom, start);
-    }
+    dfs(v[top], start);
 }
 
 int main(){
     cin >> N;
+
     for(int i=1; i<N+1; i++){
         cin >> v[i];
-        if(i == v[i]) same.push_back(i);
     }
 
     for(int i=1; i<N+1; i++){
-        if(!visited[i]){
-            dfs(i, i);
-            if(!in_cycle){
-
-            }
-        }
+        memset(visited, 0, sizeof(visited));
+        dfs(i, i);
     }
 
+    cout << cycle.size() << "\n";
+    for(int i=0; i<cycle.size(); i++){
+        cout << cycle[i] << "\n";
+    }
+    
 }
--- BOJ/tempCodeRunnerFile.cpp
+++ BOJ/tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
 
+vector<int> same;
