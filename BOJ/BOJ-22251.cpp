36b4df9b8b7da337890dab9bb5144325bd71bef3
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-18 00:46:56 +0900

    BOJ 22251 코테 유형 구현 백트래킹 - gold 5

--- BOJ/BOJ-22251.cpp
+++ BOJ/BOJ-22251.cpp
@@ -1,5 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N,K,P,X;
+set<int> res;
+int change[10][10] = {
+    {0,4,3,3,4,3,2,3,1,2}, //0
+    {4,0,5,3,2,5,6,1,5,4}, //1
+    {3,5,0,2,5,4,3,4,2,3}, //2
+    {3,3,2,0,3,2,3,2,2,1}, //3
+    {4,2,5,3,0,3,4,3,3,2}, //4
+    {3,5,4,2,3,0,1,4,2,1}, //5
+    {2,6,3,3,4,1,0,5,1,2}, //6
+    {3,1,4,2,3,4,5,0,4,3}, //7
+    {1,5,2,2,3,2,1,4,0,1}, //8
+    {2,4,3,1,2,1,2,3,1,0} //9
+};
+void hoseok(int n, int k, int p, string x){
+    if(p < 0) return;
+    if(k == K) return;
+    //cout << p;
+    int target = x[k] - '0';
+    //cout << target << "\n";
+
+    for(int i=0; i<10; i++){
+        //cout << i;
+        if(change[target][i] <= p){
+            x[k] = i + '0';
+            int nn = stoi(x);
+            if(nn <= n){
+                //cout << nn << "\n";
+                res.insert(nn);
+            }
+            hoseok(n, k + 1, p - change[target][i], x);
+            x[k] = target + '0';
+        }
+    }
+}
+
 int main(){
-    
+    cin >> N >> K >> P >> X;
+    string x_str = to_string(X);
+    int tmp = K - x_str.size();
+    for(int i=0; i<tmp; i++){
+        x_str.insert(0, "0");
+    }
+    //cout << x_str;
+    hoseok(N, 0, P, x_str);
+    res.erase(X);
+    res.erase(0);
+    cout << res.size() << "\n";
 }
--- BOJ/christmas.cpp
+++ BOJ/christmas.cpp
@@ -1,39 +1,8 @@
 #include <iostream>
 using namespace std;
-
-void drawChristmasTree(int height) {
-    // 트리 상단
-    for (int i = 1; i <= height; ++i) {
-        // 공백 출력
-        for (int j = 1; j <= height - i; ++j) {
-            cout << " ";
-        }
-        // 별 출력
-        for (int k = 1; k <= (2 * i - 1); ++k) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    // 트리 밑단 (줄기)
-    for (int i = 1; i <= height / 3; ++i) {
-        for (int j = 1; j < height; ++j) {
-            cout << " ";
-        }
-        cout << "|||" << endl; // 줄기 부분
-    }
-}
-
-int main() {
-    int height;
-    cout << "크리스마스 트리의 높이를 입력하세요: ";
-    cin >> height;
-
-    if (height < 3) {
-        cout << "트리의 높이는 최소 3 이상이어야 합니다." << endl;
-    } else {
-        drawChristmasTree(height);
-    }
-
-    return 0;
+int main(){
+    string str;
+    str = "123456";
+    int i = str[2] - '0';
+    cout << i;
 }
