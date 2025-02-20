5267a3375634c0d2cc18558247326c53767804d9
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-21 01:25:43 +0900

    BOJ 2631 dp - gold 4

--- BOJ/BOJ-2631.cpp
+++ BOJ/BOJ-2631.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
-vector<int> v(N);
 int main(){
     cin >> N;
+    vector<int> v(N);
+    vector<int> dp(N, 1);
     for(int i=0; i<N; i++){
         cin >> v[i];
     }
+
+    for(int i=1; i<N; i++){
+        for(int j=0; j<i; j++){
+            if(v[i] > v[j]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << N - *max_element(dp.begin(), dp.end());
 }
--- BOJ/tempCodeRunnerFile.cpp
+++ BOJ/tempCodeRunnerFile.cpp
@@ -1,2 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main(){
+    cin >> N;
+    vector<int> v(N);
+    vector<int> dp(N);
+    for(int i=0; i<N; i++){
+        cin >> v[i];
+    }
 
-                if(v[k] < y) cnt ++;
+    for(int i=1; i<N; i++){
+        for(int j=0; j<i; j++){
+            if(v[i] > v[j]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << N - *max_element(dp.begin(), dp.end());
+}
