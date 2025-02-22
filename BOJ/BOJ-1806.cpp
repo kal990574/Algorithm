567ec72a544db1aac885a035aad0f08477f203cd
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-22 23:37:25 +0900

    BOJ 1806 - gold 4

---
+++ BOJ/BOJ-1806.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, S;
+int arr[100001];
+int main(){
+    cin >> N >> S;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    
+}
--- BOJ/christmas.cpp
+++ BOJ/christmas.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    string str;
-    str = "123456";
-    int i = str[2] - '0';
-    cout << i;
+    int arr[3] = {1, };
+    cout << arr[2];
 }
