fa9f57c9e159ef7d7e3303a7410a531fbc3b4245
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-23 00:29:03 +0900

    BOJ 1806 투 포인터 - gold 4

--- BOJ/BOJ-1806.cpp
+++ BOJ/BOJ-1806.cpp
@@ -7,5 +7,25 @@ int main(){
     for(int i=0; i<N; i++){
         cin >> arr[i];
     }
-    
+    int start = 0;
+    int end = 0;
+    int min_cnt = INT_MAX;
+    int sum = 0;
+    while(end <= N){
+        int cnt = end - start;
+        //cout << start << " " << end << "\n";
+        if(sum >= S){
+            min_cnt = min(min_cnt, cnt);
+            sum -= arr[start];
+            start++;
+        }
+        else{
+            sum += arr[end];
+            end ++;
+        }
+    }
+    if(min_cnt == INT_MAX) cout << 0;
+    else{
+        cout << min_cnt << "\n";
+    }
 }
