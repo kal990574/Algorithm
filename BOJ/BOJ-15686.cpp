d344bc613bd09ad71e91f04aff8a6a252001f1cd
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-23 13:48:14 +0900

    BOJ 15686 순열 백트래킹 - gold 5

--- BOJ/BOJ-15686.cpp
+++ BOJ/BOJ-15686.cpp
@@ -1,84 +1,61 @@
-@ -0,0 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, M;
-int maps[51][51];
-int visited[51][51];
-int dist[51][51];
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,-1,1};
-vector<int> chik;
-int cnt = 0;
-int res;
-
-bool chk(int x, int y){
-    if(x >= 1 && x<= N && y >=1 && y<=N && !visited[x][y] && maps[x][y] != 2) return true;
-    else return false;
-}
-void check_distance(int x, int y){
-
-}
-void bfs(int x, int y){
-    memset(visited,0,sizeof(visited));
-    memset(dist,0,sizeof(dist));
-    queue<pair<int, int> > q;
-    q.push(make_pair(x,y));
-    visited[x][y] = 1;
-    int min_value = 9999;
-    while(!q.empty()){
-        int sx = q.front().first;
-        int sy = q.front().second;
-        q.pop();
-
-        for(int i=0; i<4; i++){
-            int nx = sx + dx[i];
-            int ny = sy + dy[i];
-
-            if(nx >= 1 && nx <= N && ny >= 1 && ny <= N && maps[nx][ny] == 1 && !dist[nx][ny]){
-            //cout << "MM";
-            int dis = abs(x-nx) + abs(y-ny);
-            min_value += dis;
-            //min_value = min(dis, min_value);
-            dist[nx][ny] = 1;
+vector<vector<int> > maps(51, vector<int>((51)));
+vector<pair<int, int> > chik;
+vector<pair<int, int> > home;
+int ans = INT_MAX;
+/*
+5 3
+0 0 1 0 0
+0 0 2 0 1
+0 1 2 0 0
+0 0 1 0 0
+0 0 0 0 2
+*/
+//조합의 경우의수 구한 후
+//절대값 거리 계산
+//그 중 최소 값 도출
+//방식은 재귀, 백트래킹 활용
+void dfs(int idx, vector<pair<int, int> >& perm){
+    if(perm.size() == M){
+        int sum = 0;
+        for(int i=0; i<home.size(); i++){
+            int min_dist = INT_MAX;
+            for(int j=0; j<perm.size(); j++){
+                int tmp = abs(home[i].first - perm[j].first) + abs(home[i].second - perm[j].second);
+                min_dist = min(min_dist, tmp);
             }
-            if(chk(nx,ny)){
-                //cout << nx << " " << ny << "\n";
-                q.push(make_pair(nx, ny));
-                visited[nx][ny] = 1;
-            }
+            sum += min_dist;
         }
+        ans = min(sum, ans);
+        return ;
     }
-    chik.push_back(min_value);
-    cnt ++;
+
+    for(int i=idx; i<chik.size(); i++){
+        perm.push_back(make_pair(chik[i].first, chik[i].second));
+        dfs(i+1, perm);
+        perm.pop_back();
+    }
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     cin >> N >> M;
     for(int i=1; i<=N; i++){
         for(int j=1; j<=N; j++){
             cin >> maps[i][j];
-        }
-    }
-    /*
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            if(maps[i][j] == 2){
-                check_distance(i,j);
+            if(maps[i][j] == 1){
+                home.push_back(make_pair(i, j));
             }
-        }
-    }
-    */
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            if(maps[i][j] == 2 && !visited[i][j]){
-                bfs(i,j);
+            if(maps[i][j] == 2){
+                chik.push_back(make_pair(i, j));
             }
         }
     }
 
-    for(int i=0; i<chik.size(); i++) cout << chik[i] << " ";
-    sort(chik.begin(), chik.end());
-    for(int i=0; i<M; i++){
-        res += chik[i];
-    }
-    cout << res;
+    vector<pair<int, int> > per;
+    dfs(0, per);
+    cout << ans;
 }
