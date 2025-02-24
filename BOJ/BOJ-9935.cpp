5836368b1c1321ac1671171c8e08711e27723e1f
mambasa <101706131+kal990574@users.noreply.github.com>
2025-02-24 23:17:31 +0900

    BOJ 9935 스택 - gold 4

--- BOJ/BOJ-9935.cpp
+++ BOJ/BOJ-9935.cpp
@@ -3,17 +3,37 @@ using namespace std;
 string str;
 string bomb;
 vector<int> fir;
+stack<char> s;
 // maps<char>
+//입력받으면서 stack에 넣으며 폭팔 판정
 int main(){
     cin >> str >> bomb;
-    for(int i=0; i<str.size(); i++){
-        if(str[i] == bomb[0]){
-            fir.push_back(i);
+    for(int i=str.size()-1; i>str.size()-bomb.size(); i--){
+        s.push(str[i]);
+        //cout << str[i];
+    }
+    for(int i=str.size()-bomb.size(); i>=0; i--){
+        string tmp;
+        s.push(str[i]);
+        for(int j=0; j<bomb.size(); j++){
+            if(!s.empty()){
+                tmp += s.top();
+                s.pop();
+            }
+            if(j == bomb.size() - 1 && tmp != bomb){
+                for(int k=tmp.size()-1; k>=0; k--){
+                    s.push(tmp[k]);
+                }
+            } 
         }
     }
-    for(int i=0; i<fir.size(); i++){
-        for(int j=fir[i]; j<fir[i] + bomb.size(); j++){
-            if(str[j] == bomb[])
-        }
+    string ans;
+    while(!s.empty()){
+        ans+=s.top();
+        s.pop();
     }
+    if(ans.empty()){
+        cout << "FRULA";
+    }
+    else cout << ans;
 }
