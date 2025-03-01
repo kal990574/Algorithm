1d1211628bcf176a07a48ab05ab6587c9356913f
mambasa <101706131+kal990574@users.noreply.github.com>
2025-03-02 02:37:16 +0900

    BOJ 2110 매개변수 탐색 - gold 4

---
+++ BOJ/BOJ-2110.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, C;
+int main(){
+    // 이분탐색
+    // 파라메트릭 서치
+    // 매개변수 탐색 -> 목표 값을 정해두고 탐색
+    cin >> N >> C;
+    vector<int> home(N);
+    vector<vector<int> > dp(N, vector<int>(2));
+    for(int i=0; i<N; i++){
+        cin >> home[i];  
+    }
+    sort(home.begin(), home.end());
+    int start = 1;
+    int end = home[N-1] - home[0];
+    //int start_index = 0;
+    //int end_index = N;
+    int mid = (home[start]+home[end])/2;
+    int pivot = 0;
+    C-=1;
+    int h = C;
+    int res = 0;
+    while(start <= end){
+        int ori = h;
+        int mid = (start+end) / 2;
+        // 설치 완료
+        for(int i=pivot; i<N; i++){
+            if(home[i] >= home[pivot] + mid){
+                pivot = i;
+                h -= 1;
+                //cout << "mid:" << mid <<" pivot:" << pivot << " res:" << res << " h:" << h << "\n";
+                break;
+            }
+        }
+        // 설치 모두 완료
+        if(h == 0){
+            //cout << "H" << "\n";
+            res = max(res, mid);
+            start = mid + 1;
+            pivot = 0;
+            h = C;
+        }
+
+        // 설치 실패
+        else if(ori == h){
+            end = mid - 1;
+            h = C;
+            pivot = 0;
+        }
+    }
+    cout << res << "\n";
+}
