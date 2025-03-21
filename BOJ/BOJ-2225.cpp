b6f5b237600f0916b3bc0331b1167a39be16e2c7
mambasa <101706131+kal990574@users.noreply.github.com>
2025-03-22 02:18:30 +0900

    BOJ 2225 dp 미완성 - g5

--- BOJ/BOJ-2225.cpp
+++ BOJ/BOJ-2225.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N, K;
+const int MOD = 1000000000;
+int dp[201][201] = {0};
 int main(){
+    cin >> N >> K;
+    for(int i=0; i<=N; i++){
+        dp[1][i] = 1;
+    }
+    // dp[K][N]
+    for (int i = 2; i <= K; i++) {
+        for (int j = 0; j <= N; j++) {
+            dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+        }
+    }
     
+    cout << dp[K][N] % MOD << "\n";
 }
