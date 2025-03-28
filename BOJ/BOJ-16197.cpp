809f91733f2a9180ac4bc13f8915960f6d9afcd9
mambasa <101706131+kal990574@users.noreply.github.com>
2025-03-28 14:26:51 +0900

    16197 bfs - g4

--- BOJ/BOJ-16197.cpp
+++ BOJ/BOJ-16197.cpp
@@ -1,64 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int N, M;
 vector<vector<char> > maps(20, vector<char>(20));
 vector<pair<int, int> > coin;
-bool visited[20][20][20][20];
+int visited[20][20][20][20];
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
-int res = 100000;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,-1,0,1};
+int res = 10000;
 
-void dfs(int x1, int y1, int x2, int y2, int depth) {
-    if (depth > 10) return;
+void bfs(){
+    queue<pair<pair<pair<int, int>, pair<int, int> >, int> > q;
+    q.push(make_pair(make_pair(make_pair(coin[0].first, coin[0].second), make_pair(coin[1].first, coin[1].second)), 0));
 
-    if ((x1 < 0 || x1 >= N || y1 < 0 || y1 >= M) && (x2 < 0 || x2 >= N || y2 < 0 || y2 >= M)) return;
+    while(!q.empty()){
+        int sx1 = q.front().first.first.first;
+        int sy1 = q.front().first.first.second;
+        int sx2 = q.front().first.second.first; 
+        int sy2 = q.front().first.second.second;
+        int dist = q.front().second;
+        q.pop();
+        visited[sx1][sy1][sx2][sy2] = 1;
 
-    if(x1<0||x2<0||y1<0||y2<0||x1>=N||x2>=N||y1>=M||y2>=M){
-        res = min(res, depth);
-        //cout << "res: " << res;
-        return;
-    }
+        for(int i=0; i<4; i++){
+            int nx1 = sx1 + dx[i];
+            int ny1 = sy1 + dy[i];
+            int nx2 = sx2 + dx[i];
+            int ny2 = sy2 + dy[i];
 
-    if (visited[x1][y1][x2][y2]) return;
+            //cout << nx1 << " " << ny1 << " " << nx2 << " " << ny2 << "\n"; 
 
-    visited[x1][y1][x2][y2] = true;
+            if((nx1<0||nx1>=N||ny1<0||ny1>=M) && (nx2<0||nx2>=N||ny2<0||ny2>=M)) continue;
 
-    for (int i = 0; i < 4; i++) {
-        int nx1 = x1 + dx[i];
-        int ny1 = y1 + dy[i];
-        int nx2 = x2 + dx[i];
-        int ny2 = y2 + dy[i];
+            if(nx1<0||nx2<0||ny1<0||ny2<0||nx1>=N||nx2>=N||ny1>=M||ny2>=M){
+                res = min(res, dist + 1);
+                //cout << "res: " << res;
+                break;
+            }
 
-        if (nx1 >= 0 && nx1 < N && ny1 >= 0 && ny1 < M && maps[nx1][ny1] == '#') {
-            nx1 = x1;
-            ny1 = y1;
-        }
-        if (nx2 >= 0 && nx2 < N && ny2 >= 0 && ny2 < M && maps[nx2][ny2] == '#') {
-            nx2 = x2;
-            ny2 = y2;
-        }
+            if((maps[nx1][ny1] == '.' || maps[nx1][ny1] == 'o') && (maps[nx2][ny2] == '.' || maps[nx2][ny2] == 'o') && !visited[nx1][ny1][nx2][ny2] && !visited[nx1][ny1][nx2][ny2]){
+                q.push(make_pair(make_pair(make_pair(nx1, ny1), make_pair(nx2, ny2)), dist+1));
+                visited[nx1][ny1][nx2][ny2] = 1;
+            }
 
-        dfs(nx1, ny1, nx2, ny2, depth + 1);
-    }
+            if(maps[nx1][ny1] == '#' && maps[nx2][ny2] != '#' && !visited[sx1][sy1][nx2][ny2]){
+                q.push(make_pair(make_pair(make_pair(sx1, sy1), make_pair(nx2, ny2)), dist+1));
+                visited[sx1][sy1][nx2][ny2] = 1;
+            }
 
-    visited[x1][y1][x2][y2] = false; 
-}
-
-int main() {
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> maps[i][j];
-            if (maps[i][j] == 'o') {
-                coin.push_back(make_pair(i,j));
+            if(maps[nx2][ny2] == '#' && maps[nx1][ny1] != '#' && !visited[nx1][ny1][sx2][sy2]){
+                q.push(make_pair(make_pair(make_pair(nx1, ny1), make_pair(sx2, sy2)), dist+1));
+                visited[nx1][ny1][sx2][sy2] = 1;
             }
         }
     }
+}
 
-    dfs(coin[0].first, coin[0].second, coin[1].first, coin[1].second, 0);
-
-    if (res > 10) res = -1;
+int main(){
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> maps[i][j];
+            if(maps[i][j] == 'o') coin.push_back(make_pair(i,j));
+        }
+    }
+    bfs();
+    if(res > 10) res = -1;
     cout << res << "\n";
 }
