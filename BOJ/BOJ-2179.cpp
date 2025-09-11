4de54671198c144e0a8589c9c6f0f2af2db248b4
mambasa <101706131+kal990574@users.noreply.github.com>
2025-09-11 17:02:08 +0900

    BOJ 2179 map - g4 clear

--- BOJ/BOJ-2179.cpp
+++ BOJ/BOJ-2179.cpp
@@ -1,13 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int N;
-    cin >> N;
-    int max_len = 0;
-    vector<string> arr(N);
-    for(int i=0; i<N; i++) cin >> arr[i];
-    unordered_map<string, int> mp;
-    for(int i=0; i<N; i++){
-        
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N; cin >> N;
+    vector<string> words(N);
+    for (int i = 0; i < N; i++) cin >> words[i];
+
+    unordered_map<string, vector<int>> mp;
+    mp.reserve(N * 50); 
+
+    for (int i = 0; i < N; i++) {
+        string pref = "";
+        for (char c : words[i]) {
+            pref.push_back(c);
+            auto &vec = mp[pref];
+            if (vec.size() < 2) {
+                vec.push_back(i);
+            }
+        }
     }
+
+    int bestLen = -1;
+    pair<int,int> best = {1e9, 1e9};
+
+    for (auto &kv : mp) {
+        auto &vec = kv.second;
+        if (vec.size() < 2) continue;
+
+        int len = kv.first.size();
+        int a = vec[0], b = vec[1];
+        int S = a, T = b;
+        if (S > T) swap(S,T);
+
+        if (len > bestLen) {
+            bestLen = len;
+            best = {S,T};
+        } else if (len == bestLen) {
+            if (S < best.first || (S == best.first && T < best.second)) {
+                best = {S,T};
+            }
+        }
+    }
+
+    cout << words[best.first] << "\n" << words[best.second] << "\n";
 }
