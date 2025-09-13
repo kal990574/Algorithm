ead89482bf8e38bc7ef69ff791b732d3c6200e74
mambasa <101706131+kal990574@users.noreply.github.com>
2025-09-13 16:26:04 +0900

    BOJ 1744 greedy condition - g4

--- BOJ/BOJ-1744.cpp
+++ BOJ/BOJ-1744.cpp
@@ -1,5 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    vector<pair<int, int>> diff(N-1);
+    vector<int> ban(1, -1);
+    int res = 0;
+    for(int i=0; i<N; i++){
+        cin >> v[i];
+    }
+
+    if(N == 1){
+        cout << v[0];
+        return 0;
+    }
+
+    sort(v.begin(), v.end(), greater<>());
+
+    for(int i=0; i<N-1; i++){
+        diff[i].first = (v[i] * v[i+1]) - (v[i] + v[i+1]);
+        diff[i].second = i;
+    }
+    sort(diff.begin(), diff.end(), greater<>());
+
+    for(int i=0; i<N-1; i++){
+        if(diff[i].first > 0 && find(ban.begin(), ban.end(), diff[i].second) == ban.end() && find(ban.begin(), ban.end(), diff[i].second + 1) == ban.end()){
+            res += (v[diff[i].second] * v[diff[i].second + 1]);
+            ban.push_back(diff[i].second);
+            ban.push_back(diff[i].second + 1);
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        if(find(ban.begin(), ban.end(), i) == ban.end()){
+            res += v[i];
+        }
+    }
+    cout << res << "\n";
 }
